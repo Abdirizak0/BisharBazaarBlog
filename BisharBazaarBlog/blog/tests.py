d39d190486065e0e84382882ca9cc97bb8e77020
Abdirizak Omar <abdulrazakbishar@gmail.com>
2024-06-11 22:40:59 +0000

    initual

--- BisharBazaarBlog/blog/tests.py
+++ BisharBazaarBlog/blog/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your tests here.
+class PostTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.post = Post.objects.create(title='Test Title', text='Test text', author=self.user)
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('post_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Title')
+        self.assertTemplateUsed(response, 'blog/post_list.html')
+
+    def test_post_detail_view(self):
+        response = self.client.get(reverse('post_detail', args=[self.post.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Title')
+        self.assertTemplateUsed(response, 'blog/post_detail.html')
